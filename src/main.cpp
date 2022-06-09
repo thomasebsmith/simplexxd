afec3003e9ba9b45fccb7ea8a8e67ded3c75f39e
Thomas Smith <thsm@umich.edu>
2022-06-08 23:54:09 -0400

    Improve code quality

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,13 @@
 
 constexpr static std::size_t byte_max_value = 256;
 
-using hex_array = std::array<unsigned char, byte_max_value * 2>;
-
 constexpr static std::array<unsigned char, 16> hex_digits{
   '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
 };
 
-constexpr hex_array create_hex_array() {
+using hex_array = std::array<unsigned char, byte_max_value * 2>;
+
+constexpr hex_array create_hex_digits_by_byte() {
   hex_array result{};
   for (std::size_t byt = 0; byt < byte_max_value; ++byt) {
     result[byt * 2] = hex_digits[(byt & 0xf0) >> 4];
@@ -20,7 +20,7 @@ constexpr hex_array create_hex_array() {
   return result;
 }
 
-constexpr static hex_array bytes = create_hex_array();
+constexpr static hex_array hex_digits_by_byte = create_hex_digits_by_byte();
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -34,15 +34,15 @@ int main(int argc, char **argv) {
   file_mapping file{argv[1], read_only};
   mapped_region region{file, read_only};
 
-  auto addr_begin = (unsigned char *) region.get_address();
+  auto addr_begin = static_cast<unsigned char *>(region.get_address());
   auto addr_end = addr_begin + region.get_size();
 
   // Read the entire memory-mapped region, outputting two hex characters for
   // each byte.
   for (auto addr = addr_begin; addr < addr_end; ++addr) {
     unsigned char ch = *addr;
-    putchar_unlocked(bytes[ch * 2]);
-    putchar_unlocked(bytes[ch * 2 + 1]);
+    putchar_unlocked(hex_digits_by_byte[ch * 2]);
+    putchar_unlocked(hex_digits_by_byte[ch * 2 + 1]);
   }
 
   return 0;
